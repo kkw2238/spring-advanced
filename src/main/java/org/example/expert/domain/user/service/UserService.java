0648625d86ab1ea4bc651f706cf7f2423ed2cba4
kkw2238 <kkw2238@naver.com>
2024-09-09 20:30:00 +0900

    레벨 1-3: 메서드 분리
    1. Password 관련 Utility 내용이 담긴 PasswordUtil Class추가.
    2. 비밀번호가 유효한지 확인하는 내용을 정규식으로 구현

--- src/main/java/org/example/expert/domain/user/service/UserService.java
+++ src/main/java/org/example/expert/domain/user/service/UserService.java
@@ -2,6 +2,7 @@ package org.example.expert.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.expert.config.PasswordEncoder;
+import org.example.expert.config.PasswordUtil;
 import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.user.dto.request.UserChangePasswordRequest;
 import org.example.expert.domain.user.dto.response.UserResponse;
@@ -17,7 +18,13 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final PasswordUtil passwordUtil;
 
+    /**
+     * userId에 해당하는 유저 정보를 반환하는 메서드
+     * @param userId 찾고자 하는 userId
+     * @return 해당 userId를 갖고 있는 유저 객체
+     */
     public UserResponse getUser(long userId) {
         User user = userRepository.findById(userId).orElseThrow(() -> new InvalidRequestException("User not found"));
         return new UserResponse(user.getId(), user.getEmail());
@@ -25,23 +32,28 @@ public class UserService {
 
     @Transactional
     public void changePassword(long userId, UserChangePasswordRequest userChangePasswordRequest) {
-        if (userChangePasswordRequest.getNewPassword().length() < 8 ||
-                !userChangePasswordRequest.getNewPassword().matches(".*\\d.*") ||
-                !userChangePasswordRequest.getNewPassword().matches(".*[A-Z].*")) {
+        /* 수정된 코드 : 비밀번호 확인 메서드 분할
+            비밀번호가 조건에 맞는지 확인
+         */
+        if (passwordUtil.isValidPassword(userChangePasswordRequest.getNewPassword())) {
             throw new InvalidRequestException("새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.");
         }
 
+        // 해당 userId를 가진 유저 판별
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new InvalidRequestException("User not found"));
 
+        // 새 비밀번호와 현재 비밀번호 비교
         if (passwordEncoder.matches(userChangePasswordRequest.getNewPassword(), user.getPassword())) {
             throw new InvalidRequestException("새 비밀번호는 기존 비밀번호와 같을 수 없습니다.");
         }
 
+        // 기존 비밀번호와 현재 비밀 번호가 맞는지 확인
         if (!passwordEncoder.matches(userChangePasswordRequest.getOldPassword(), user.getPassword())) {
             throw new InvalidRequestException("잘못된 비밀번호입니다.");
         }
 
+        // 비밀번호 변경
         user.changePassword(passwordEncoder.encode(userChangePasswordRequest.getNewPassword()));
     }
 }
